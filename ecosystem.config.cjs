356ce420f07889dbe9aae7003b36b6b29864b030
Mohammad Kiani <60611017+mkiani12@users.noreply.github.com>
2024-07-01 13:19:49 +0330

    ok

---
+++ ecosystem.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: "FezClient", // Replace with your Nuxt application name
+      script: "./.output/server/index.mjs", // Path to the Nuxt server entry point
+      exec_mode: "cluster", // Leverage Node.js cluster module for multi-process performance
+      instances: "max", // Run the maximum number of worker processes allowed by your system
+      env: {
+        // Optional environment variables for your application
+        NODE_ENV: "production", // Set the environment to production
+        // Add other environment variables as needed
+      },
+    },
+  ],
+};
